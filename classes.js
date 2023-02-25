1988bcb1090c2cda7a60c56ef5aa6e71074dfa46
Baran Dogan <phscloq@gmail.com>
2023-02-25 13:55:08 +0300

    project property added inside of task

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ class Todo {
         this.dueDate = dueDate;
         this.priority = priority;
         this.completed = false;
+        this.project = null;
         this.id = Math.random().toString(36).substring(2, 9);
     }
     changePriority(newPriority) {
@@ -45,17 +46,28 @@ class Project {
 
 
 const task1= new Todo('42', 'The meaning of 42', '2021-01-01', 'Low');
-const task2= new Todo('Read the article', 'Take notes and highlight', '2021-01-01', 'High');
+const task2= new Todo('Read the article', 'Take notes and highlight', '2021-01-01', 'Medium');
 const task3= new Todo('Finish the project', 'Finish the project', '2021-01-01', 'High');
+const task4= new Todo('write the paper', 'Finish the paper', '2021-01-01', 'Medium');
+const task5= new Todo('Finish the ToDo App', 'Finish the todo app', '2021-01-01', 'High');
+const task6= new Todo('Get Paid', 'lets earn some moneyyy', '2021-02-15', 'High');
 const project1 = new Project('Study');
 const project2 = new Project('Hobby');
 const project3 = new Project('Work');
-project1.addTodo(task1);
+
 project1.addTodo(task2);
-project2.addTodo(task2);
+project1.addTodo(task4);
+project2.addTodo(task1);
 project3.addTodo(task3);
-project3.addTodo(task1);
-project3.addTodo(task2);
+project3.addTodo(task5);
+project3.addTodo(task6);
+
+project1.todos[0].project=project1.name;
+project1.todos[1].project=project1.name;
+project2.todos[0].project=project2.name;
+project3.todos[0].project=project3.name;
+project3.todos[1].project=project3.name;
+project3.todos[2].project=project3.name;
 
 console.log(Project.projects);
 /* Project.getProjects(); */
