ae361b9683199c274a942479255a49ba82dbc180
Baran Dogan <phscloq@gmail.com>
2023-03-08 16:30:52 +0300

    The problem regarding click events fixed: while a form is opened, tasks will be unclickable

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './style.css'
 import { Todo, Project } from "../classes";
 import {taskNumbers, deleteProject, deleteTask, taskComplete, getIndex, projectEditCancel, projectUpdate} from "../functions";
 import { format, getDate } from "date-fns";
-import {displayContent, homeTab, newProject, todayTab, upcomingTab} from "../content";
-import {projectSetting, taskEdit, projectEdit} from "../opens";
+import {displayContent, homeTab, newProject, todayTab, upcomingTab, mobileAddPrj} from "../content";
+import {projectSetting, taskEdit, projectEdit, mobileNewProject} from "../opens";
 
 pageLoad();
 const prList= document.getElementById('projects-list');
@@ -22,17 +22,24 @@ const today = document.getElementById('today');
 const upcoming = document.getElementById('upcoming');
 const projectSection = document.getElementById('projectSectionTitle');
 const sectionTitle = document.getElementById('section-title');
+const openNewProjectForm = document.getElementsByClassName('mAddPrjBtn');
 projectSection.addEventListener('click', () => {
     const computedStyle = getComputedStyle(prList);
     if(computedStyle.display === 'none'){
         prList.style.display = 'flex';
-        document.querySelector('.newProjectFormDiv').style.display = 'block';
+        if(openNewProjectForm){console.log("button exists");
+    
+    }
+        mobileAddPrj();
     }
     else if(computedStyle.display === 'flex'){
         prList.style.display = 'none';
         document.querySelector('.newProjectFormDiv').style.display = 'none';}
- });  
-
+ });
+ //*******NEW PROJECT FORM DISPLAY *******/  
+ if(document.querySelector('.mAddPrjBtn')){
+    console.log("button exists");
+}
 
 
 
@@ -55,7 +62,7 @@ upcoming.addEventListener('click', () => {
     upcomingTab();
 });
 
-//*******PROJECT ADDEDVENTLISTENERS*******
+//*******DISPLAY A PROJECT*******
 document.getElementById('projects-list').addEventListener('click', (event) => {
 
   if (event.target.classList.contains('project')) {
@@ -66,7 +73,7 @@ document.getElementById('projects-list').addEventListener('click', (event) => {
 
     const {tasksHTML, taskAddHtml}= displayContent(getIndex(projectId));
     document.querySelector('#task-container').innerHTML = tasksHTML;
-   document.querySelector('#task-container').appendChild(taskAddHtml);
+   document.querySelector('#content-container').appendChild(taskAddHtml);
    
   }
   else if(event.target.classList.contains('prjName')){
@@ -76,12 +83,12 @@ document.getElementById('projects-list').addEventListener('click', (event) => {
 
     const {tasksHTML, taskAddHtml}= displayContent(getIndex(projectId));
     document.querySelector('#task-container').innerHTML = tasksHTML;
-   document.querySelector('#task-container').appendChild(taskAddHtml);
+   document.querySelector('#content-container').appendChild(taskAddHtml);
   }
 });
 
 
-                //*******FORM ADDEDVENTLISTENERS*******
+ //*******FORM ADDEDVENTLISTENERS*******
 //Cancel Button
 document.getElementById('formBtnCancel').addEventListener('click', (e) => {
     e.preventDefault();
@@ -92,7 +99,7 @@ document.getElementById('formBtnCancel').addEventListener('click', (e) => {
     }
     });
 
-        //New Task Add Form --
+//*******SUBMIT TASK*******
         //takes the inputs and creates a new task object and adds it to the project's todo array
         //and update the content
                   document.querySelector('.formBtnAdd').addEventListener('click', (event)=>{
@@ -122,9 +129,9 @@ document.getElementById('formBtnCancel').addEventListener('click', (e) => {
                         localStorage.setItem('projects', JSON.stringify(Project.projects));
                         //from the section title, find the project index
 
-                        const {tasksHTML, taskAddHtml}= displayContent(projectIndex);
+                        const {tasksHTML}= displayContent(projectIndex);
                         document.querySelector('#task-container').innerHTML = tasksHTML;
-                        document.querySelector('#task-container').appendChild(taskAddHtml);
+                        
 
                         document.getElementById('formDiv').style.display = 'none';
                         taskNumbers(Project.projects[projectIndex]);
@@ -132,7 +139,7 @@ document.getElementById('formBtnCancel').addEventListener('click', (e) => {
                  });
         
 
-//New Project Add Form --
+//*******NEW PROJECT ADD FORM*******
 document.getElementById('projectSubmit').addEventListener('click', (event)=>{
     event.preventDefault();
     const projectName = document.getElementById('projectName').value;
@@ -143,7 +150,7 @@ document.getElementById('projectSubmit').addEventListener('click', (event)=>{
    
   
 });
-
+//*******OPENS PROJECT SETTINGS*******/
 document.querySelectorAll('.projectEdit').forEach((element)=>{
 element.addEventListener('click', (event)=>{
     projectSetting(event.target);  
@@ -151,77 +158,90 @@ element.addEventListener('click', (event)=>{
 
 });
 document.querySelector('.content').addEventListener('click', (event)=>{
-        //If its not the project settings or the project edit button (so the user could open settings window)
-            if(!event.target.closest('.projectSettings') && !event.target.closest('.projectEdit')){
+    //PROJECT SETTINGS'S BUTTONS'S EVENT LISTENERS
+        if(document.getElementById('projectSettings')){
+            document.querySelector('.projectSettings').addEventListener('click', (event)=>{
+                if(event.target.id === 'deletePrjBtn'){//DELETE PROJECT
+                    const data = event.target.parentNode.getAttribute('data-pr');
+                    deleteProject(data);
+                }
+                if(event.target.id === 'editPrjBtn'){//OPENS THE EDIT FORM
+                    console.log(event.target);
+                    console.log('clicked');
+                    projectEdit(event.target);
+                }
+            });}
+        
+     
+//CLICKING ANYWHERE OUTSIDE OF THE OPENED FORM WILL
+        //CLOSE THE PROJECT SETTINGS FORM
                 if(document.querySelector('.projectSettings')){
+                    document.querySelector('#mainSection').style.pointerEvents = 'none'; //make tasks unclicable
+                    if(!event.target.closest('.projectSettings') && !event.target.closest('.projectEdit')){
                 document.querySelector('.projectSettings').remove();
                 document.getElementById('content-container').style.opacity = '1';
-            }}
-   //Clicking anywhere outside of task edit form will close the task edit form and saves the changes
-        if(!event.target.closest('.taskSettings') && !event.target.closest('#taskEditForm')){
-            if(document.querySelector('.taskEdit')){
-            document.querySelector('.taskEdit').style.display = 'none';
-                const taskTitle = document.getElementById('taskEditTitle').value;
-                const taskDescription = document.getElementById('taskEditDesc').value;
-                const taskDueDate = document.getElementById('taskEditDueDate').value;
-                const formatedDueDate = format(new Date(taskDueDate), "dd/MM/yyyy");
-                const taskPriority = document.getElementById('taskEditPriority').value;
-                const taskId = document.getElementById('taskEdit').getAttribute('data');
-                const projectName = document.getElementById('taskEdit').getAttribute('data-project');
-                document.querySelector('.taskEdit').remove();
-                const {projectIndex, taskIndex} = getIndex(projectName, taskId);
-
-
-                Project.projects[projectIndex].todos[taskIndex].title = taskTitle;
-                Project.projects[projectIndex].todos[taskIndex].description = taskDescription;
-                Project.projects[projectIndex].todos[taskIndex].dueDate = formatedDueDate;
-                Project.projects[projectIndex].todos[taskIndex].priority = taskPriority;
-                localStorage.setItem('projects', JSON.stringify(Project.projects));
-               
-                if(document.getElementById('section-title').textContent === 'Home'){homeTab();
-                 
-                }
-                else {
-                
-                const {tasksHTML, taskAddHtml}= displayContent(projectIndex);
-                document.querySelector('#task-container').innerHTML = tasksHTML;
-                document.querySelector('#task-container').appendChild(taskAddHtml);}
-            }}
-       //------------------Task Settings------------------
-       if(document.getElementById('projectSettings')){
-        document.querySelector('.projectSettings').addEventListener('click', (event)=>{
-           
-            if(event.target.id === 'deletePrjBtn'){
-     
-                const data = event.target.parentNode.getAttribute('data-pr');
-            
-                deleteProject(data);
-              }
-              if(event.target.id === 'editPrjBtn'){
-                console.log(event.target);
-                console.log('clicked');
-                projectEdit(event.target);
+                document.querySelector('#mainSection').style.pointerEvents = 'auto'; //make tasks clickable
             }
-        
-        
-        });}
-        if(document.getElementById('projectEditDiv')){
-            document.querySelector('.projectEditDiv').addEventListener('click', (event)=>{
-                if(event.target.id === 'rename'){
-                    console.log("rename");
-                    projectUpdate(event.target);
-                    document.getElementById('content-container').style.opacity = '1';
-                }
-                if(event.target.id === 'cancel'){
-                    projectEditCancel(event.target);
-                    document.getElementById('content-container').style.opacity = '1';
-                }
+            }
+        //CLOSE THE TASK EDIT FORM & SAVES THE CHANGES
+            if(!event.target.closest('.taskSettings') && !event.target.closest('#taskEditForm')){
+                if(document.querySelector('.taskEdit')){
+                document.querySelector('.taskEdit').style.display = 'none';
+                    const taskTitle = document.getElementById('taskEditTitle').value;
+                    const taskDescription = document.getElementById('taskEditDesc').value;
+                    const taskDueDate = document.getElementById('taskEditDueDate').value;
+                    const formatedDueDate = format(new Date(taskDueDate), "dd/MM/yyyy");
+                    const taskPriority = document.getElementById('taskEditPriority').value;
+                    const taskId = document.getElementById('taskEdit').getAttribute('data');
+                    const projectName = document.getElementById('taskEdit').getAttribute('data-project');
+                    document.querySelector('.taskEdit').remove();
+                    const {projectIndex, taskIndex} = getIndex(projectName, taskId);
 
-            });
 
-        }
+                    Project.projects[projectIndex].todos[taskIndex].title = taskTitle;
+                    Project.projects[projectIndex].todos[taskIndex].description = taskDescription;
+                    Project.projects[projectIndex].todos[taskIndex].dueDate = formatedDueDate;
+                    Project.projects[projectIndex].todos[taskIndex].priority = taskPriority;
+                    localStorage.setItem('projects', JSON.stringify(Project.projects));
+                
+                    if(document.getElementById('section-title').textContent === 'Home'){homeTab();
+                    
+                    }
+                    else {
+                    
+                    const {tasksHTML}= displayContent(projectIndex);
+                    document.querySelector('#task-container').innerHTML = tasksHTML;
+                   }
+                }}
+        //CLOSE THE PROJECT EDIT FORM & SAVES THE CHANGES        
+            if(document.getElementById('projectEditDiv')){
+                document.querySelector('.projectEditDiv').addEventListener('click', (event)=>{
+                    if(event.target.id === 'rename'){
+                        console.log("rename");
+                        projectUpdate(event.target);
+                        document.getElementById('content-container').style.opacity = '1';
+                        document.querySelector('#mainSection').style.pointerEvents = 'auto';
+                    }
+                    if(event.target.id === 'cancel'){
+                        projectEditCancel(event.target);
+                        document.getElementById('content-container').style.opacity = '1';
+                        document.querySelector('#mainSection').style.pointerEvents = 'auto';
+                    }
 
-});
+                });}
+        //CLOSE THE NEW PROJECT FORM
+            if(document.querySelector('.newProjectFormDiv').style.display === 'block'){
+                document.querySelector('#mainSection').style.pointerEvents = 'none'; //make tasks unclicable
+                if(!event.target.closest('.newProjectFormDiv') && 
+                !event.target.closest('.mAddPrjBtn')&& 
+                !event.target.closest('.mAddPrjBtnIcon') && !event.target.closest('.newProjectForm')){
+                console.log('clicked outside');
+                document.querySelector('.newProjectFormDiv').style.display = 'none';
+                document.getElementById('content-container').style.opacity = '1';
+                document.querySelector('#mainSection').style.pointerEvents = 'auto';
+                }}
+    
+});//END OF CONTENT CONTAINER EVENT LISTENER************
 
 document.getElementById('task-container').addEventListener('click', (event)=>{
   
@@ -248,4 +268,8 @@ taskComplete(event.target);
 
     }
 });
-
+document.getElementById('sidebar-container').addEventListener('click', (event)=>{
+    if(event.target.classList.contains('mAddPrjBtn')|| event.target.classList.contains('mAddPrjBtnIcon')){
+        console.log('clicked');
+        mobileNewProject();
+    }});
